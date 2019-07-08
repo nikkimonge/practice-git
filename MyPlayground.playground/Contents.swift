e66d33d3f60f6e645cbb01a4443585fa7f476a43
Guess Password <meganmpuppy@gmail.com>
2019-07-08 14:55:32 -0700

    first save

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,72 @@
+import UIKit
+
+var str = "Hello, playground"
+var age = 16
+"I am \(age) years old"
+let string1 = "hello"
+let string2 = "there"
+var welcome = "\(string1) \(string2)"
+var anotherWelcome = string1 + " " + string2
+// welcome and anotherWelcome are both storing the string "hello there"
+print ("hello, World!")
+var first = "karlie"
+var  last = "kloss"
+"\(first)\(last)"
+"\(last)\(first)"
+"\(first) \(last)"
+"\(last) \(first) \(last) \(first)"
+"I love \(first) \(last)"
+var a = 12
+var b = 65
+var c = 31
+var d = 98
+Double(a+b+c+d)/4
+a+b*c/d
+(a+b)*c/d
+a+b*(c/d)
+var name = "Nikki"
+var address = "1314 Brightwood St"
+let birthday = "May 28, 2003"
+var gradeLevel = "11"
+var Age = "16"
+let eyeColor = "brown"
+var favoriteColor = "blue"
+var paid = 10.25
+20*paid
+var Celeb = "Taylor Swift"
+"Happy birthday \(Celeb)"
+var StringtoCompress = "Angeles"
+print ("\(StringtoCompress.first!)\(StringtoCompress.count-2)\(StringtoCompress.last!)")
+5<3
+12>7
+6 !=  8
+"karlie" == "karlie"
+"karlie" == " karlie"
+var luckyNum = 7
+luckyNum < 10
+luckyNum == 7
+var dogAge = 14
+if dogAge < 2 { print ("you are a puppy")}
+if dogAge == 2 { print ("you are awesome!")}
+if dogAge == 3 { print ("You are awesome!")}
+if dogAge ==  4 { print ("You are awesome!")}
+if dogAge > 12 { print ("You are elderly")}
+else if dogAge > 12 {
+    print ("You are elderly")
+}
+else {
+    print ("You are also awesome")
+}
+var hasAccount = false
+if hasAccount ==  false { print ("Please log in")}
+else {
+    print ("Please make an account")
+}
+var grade = 6
+if grade < 6 {print ("elementary school")}
+else if grade >= 6 {
+    print ("middle school")
+}
+else if grade <= 12 {
+    print ("high school")
+}
